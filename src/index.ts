c45965e408d13692aff9376ecae801dbb204766a
sayantansnl <sayantansanyal72@gmail.com>
2026-08-14 12:15:13 +0530

    Main initialized

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+import { argv } from "node:process";
+
+function main() {
+  const args = argv.slice(2);
+  if (args.length > 1) {
+    console.log("Too many arguments. Provide one URL please.");
+    process.exit(1);
+  }
+
+  if (args.length < 1) {
+    console.log("No arguments provides. Provide one URL please.");
+    process.exit(1);
+  }
+
+  console.log(`Crawling starting at ${args[0]}`);
+  process.exit(0);
+}
+
+main();
