4bb853566c78e67712939a07f8a1e29e11f82730
sayantansnl <sayantansanyal72@gmail.com>
2026-08-15 08:10:56 +0530

    Getting html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { argv } from "node:process";
 
-function main() {
+async function main() {
   const args = argv.slice(2);
   if (args.length > 1) {
     console.log("Too many arguments. Provide one URL please.");
@@ -12,8 +12,39 @@ function main() {
     process.exit(1);
   }
 
-  console.log(`Crawling starting at ${args[0]}`);
+  const url = args[0];
+
+  console.log(`Crawling starting at ${url}`);
+
+  const html = await getHTML(url);
+  console.log(html);
   process.exit(0);
+}
+
+async function getHTML(url: string) {
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "User-Agent": "Crawler/1.0",
+        "Content-Type": "text/html",
+      },
+    });
+
+    if (res.status >= 400) {
+      console.log(`Error in getting a response, code: ${res.status}`);
+      return;
+    }
+
+    const contentType = res.headers.get("content-type");
+    if (!contentType || !contentType.includes("text/html")) {
+      console.log("Got non html response");
+    }
+
+    return res.text();
+  } catch (err) {
+    console.error(`couldn't crawl site due to: ${err}`);
+  }
 }
 
 main();
